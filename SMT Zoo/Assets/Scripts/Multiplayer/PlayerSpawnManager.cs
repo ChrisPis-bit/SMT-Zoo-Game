5862961245940281305964a3dc4bc51f7bef0520
sajithad <sdulrangi@gmail.com>
2024-01-19 00:19:19 +0100

    added player positions

--- SMT Zoo/Assets/Scripts/Multiplayer/PlayerSpawnManager.cs
+++ SMT Zoo/Assets/Scripts/Multiplayer/PlayerSpawnManager.cs
@@ -7,7 +7,6 @@ using Random = System.Random;
 
 public class PlayerSpawnManager : NetworkBehaviour
 {
-    public GameObject playerPrefab;
     public Transform clientTransform;
 
     public void Start()
@@ -21,56 +20,6 @@ public class PlayerSpawnManager : NetworkBehaviour
         {
             GameObject targetObject = GameObject.FindWithTag("Player");
             targetObject.transform.position = clientTransform.position;
-        }
-    }
-
-    private async void SpawnPlayer()
-    {
-        if (IsServer)
-        {
-            GameObject player = Instantiate(playerPrefab, clientTransform.position, Quaternion.identity);
-
-            // Ensure the player object has a NetworkObject component
-            NetworkObject networkObject = player.GetComponent<NetworkObject>();
-            if (networkObject != null)
-            {
-
-                networkObject.SpawnAsPlayerObject(GenerateRandomClientId(), player);
-
-            }
-            else
-            {
-                Debug.LogError("Player prefab does not have a NetworkObject component.");
-            }
-        }
-        
-    }
-    private ulong GenerateRandomClientId()
-    {
-        Random random = new Random();
-
-        // Generate two random uint values and combine them into a ulong
-        ulong randomClientId = ((ulong)random.Next() << 32) | (uint)random.Next();
-
-        return randomClientId;
-    }
-    private async void SpawnPlayerClient()
-    {
-        // Instantiate the player prefab
-        GameObject player = Instantiate(playerPrefab, gameObject.transform.position, Quaternion.identity);
-
-        // Ensure the player object has a NetworkObject component
-        NetworkObject networkObject = player.GetComponent<NetworkObject>();
-        if (networkObject != null)
-        {
-            
-            networkObject.Spawn(player);
-
-            
-        }
-        else
-        {
-            Debug.LogError("Player prefab does not have a NetworkObject component.");
         }
     }
 }
