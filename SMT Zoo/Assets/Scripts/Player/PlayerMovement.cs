44b94b7ac07e8b427a0c0b59f0d76f076ee1f88c
sajithad <sdulrangi@gmail.com>
2024-01-18 21:29:59 +0100

    client and host configs added

--- SMT Zoo/Assets/Scripts/Player/PlayerMovement.cs
+++ SMT Zoo/Assets/Scripts/Player/PlayerMovement.cs
@@ -35,12 +35,15 @@ public class PlayerMovement : NetworkBehaviour
 
     void Update()
     {
+        if (!IsOwner)
+        {
+            return;
+        }
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
 
         bool isRunning = Input.GetKey(KeyCode.LeftShift);
         float curSpeedX = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Vertical") : 0;
-        Debug.Log("current speed " + curSpeedX);
         float curSpeedY = canMove ? (isRunning ? runSpeed : walkSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
