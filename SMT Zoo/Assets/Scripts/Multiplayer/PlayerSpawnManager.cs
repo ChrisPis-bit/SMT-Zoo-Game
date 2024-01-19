8bbc8b2b026ffbc1ed84fce727513e6fd72cb432
sajithad <sdulrangi@gmail.com>
2024-01-19 10:35:13 +0100

    player positions are correct

--- SMT Zoo/Assets/Scripts/Multiplayer/PlayerSpawnManager.cs
+++ SMT Zoo/Assets/Scripts/Multiplayer/PlayerSpawnManager.cs
@@ -7,11 +7,18 @@ using Random = System.Random;
 
 public class PlayerSpawnManager : NetworkBehaviour
 {
+    public GameObject playerPrefab;
     public Transform clientTransform;
-
+    
+    public override void OnNetworkSpawn() {
+        if (IsServer)
+            SpawnPlayerServerRpc(NetworkManager.Singleton.LocalClientId, true);
+        else
+            SpawnPlayerServerRpc(NetworkManager.Singleton.LocalClientId, false);
+    }
     public void Start()
     {
-        if (!IsOwner)
+        /*if (!IsOwner)
         {
             GameObject targetObject = GameObject.FindWithTag("Player");
             targetObject.transform.position = gameObject.transform.position;
@@ -20,6 +27,17 @@ public class PlayerSpawnManager : NetworkBehaviour
         {
             GameObject targetObject = GameObject.FindWithTag("Player");
             targetObject.transform.position = clientTransform.position;
-        }
+        }*/
+    }
+    [ServerRpc(RequireOwnership=false)] //server owns this object but client can request a spawn
+    public void SpawnPlayerServerRpc(ulong clientId, bool isHostPlayer) {
+        GameObject newPlayer;
+        if (!isHostPlayer)
+            newPlayer=Instantiate(playerPrefab, gameObject.transform.position, Quaternion.identity);
+        else
+            newPlayer=Instantiate(playerPrefab, clientTransform	.position, Quaternion.identity);
+        NetworkObject netObj=newPlayer.GetComponent<NetworkObject>();
+        newPlayer.SetActive(true);
+        netObj.SpawnAsPlayerObject(clientId,true);
     }
 }
