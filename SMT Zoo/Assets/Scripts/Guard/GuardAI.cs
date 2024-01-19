dbdeda800533d640813f7f38d12961c6c808dd57
kikidrawss <dreamwarrior2020@outlook.com>
2024-01-19 23:47:53 +0100

    Guard vs player range implemented

--- SMT Zoo/Assets/Scripts/Guard/GuardAI.cs
+++ SMT Zoo/Assets/Scripts/Guard/GuardAI.cs
@@ -6,7 +6,8 @@ using UnityEngine.AI;
 public enum GuardState
 {
     Patrol,
-    Inspect
+    Inspect,
+    Chase
 }
 
 public class GuardAI : MonoBehaviour
@@ -14,8 +15,15 @@ public class GuardAI : MonoBehaviour
     [SerializeField] private WayPointHandler _wayPointHandler;
     [SerializeField] private NavMeshAgent _agent;
     [SerializeField] private float _reachDistance = .2f;
+    [SerializeField] private float _playerGuardDistance = 5.0f;
     [SerializeField] private float _inspectTime = 3.0f;
     [SerializeField] private float _recalculateReturnPathInterval = 1.0f;
+
+    [SerializeField] private Transform player;
+    [SerializeField] private LayerMask playerLayer;
+
+    [SerializeField] private float sightRange;
+    [SerializeField] private bool playerInSightRange;
 
     private Vector3 _target;
     private uint _targetIndex = 0;
@@ -25,6 +33,11 @@ public class GuardAI : MonoBehaviour
 
     public GuardState State { get; private set; }
 
+    void Awake()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
     private void Start()
     {
         StartPatrol();
@@ -32,6 +45,18 @@ public class GuardAI : MonoBehaviour
 
     private void Update()
     {
+        // Check for sight range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, playerLayer);
+
+        if (!playerInSightRange)
+        {
+            State = GuardState.Patrol;
+        }
+        else
+        {
+            State = GuardState.Chase;
+        }
+
         switch (State)
         {
             case GuardState.Patrol:
@@ -40,7 +65,11 @@ public class GuardAI : MonoBehaviour
             case GuardState.Inspect:
                 Inspect();
                 break;
+            case GuardState.Chase:
+                ChasePlayer();
+                break;
         }
+        
     }
 
     public void StartPatrol()
@@ -97,6 +126,20 @@ public class GuardAI : MonoBehaviour
             _currentInspectTime += Time.deltaTime;
             if (_currentInspectTime >= _inspectTime)
                 StartPatrol();
+        }
+    }
+
+    private void ChasePlayer()
+    {
+        _agent.SetDestination(player.position);
+
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+
+        // If the player is too close, you can implement a behavior
+        if (distanceToPlayer < _playerGuardDistance)
+        {
+            //Show Game Over - Failed State
         }
     }
 }
