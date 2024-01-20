9e3a4dd3d52972edfca7d35f2d536a4b15a0d9f2
Chris Lokhorst <chris.lokhorst@hva.nl>
2024-01-20 15:04:26 +0100

    Added results file

--- SMT Zoo/Assets/Scripts/GameEventManager.cs
+++ SMT Zoo/Assets/Scripts/GameEventManager.cs
@@ -1,13 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class GameEventManager : NetworkBehaviour
 {
+    private const string CSV_NAME = "Results";
+
     public UnityEvent onComplete;
     public bool GameCompleted { get; private set; }
+
+    private float _time = 0;
+    private int _failCount = 0;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -22,6 +28,11 @@ public class GameEventManager : NetworkBehaviour
         }
     }
 
+    private void Update()
+    {
+        _time += Time.deltaTime;
+    }
+
     [ServerRpc(RequireOwnership = false)]
     public void WinGameServerRpc()
     {
@@ -33,5 +44,27 @@ public class GameEventManager : NetworkBehaviour
     {
         GameCompleted = true;
         onComplete?.Invoke();
+        WriteEventsToCSV();
     }
+
+    /// <summary>
+    /// Writes the current event data to a .csv file.
+    /// </summary>
+    public void WriteEventsToCSV()
+    {
+        string path = $"{Application.dataPath}/{CSV_NAME}.csv";
+
+        if (File.Exists(path))
+            File.Delete(path);
+
+        StreamWriter writer = new StreamWriter(path);
+
+        // Writes all event data to the .csv
+        writer.WriteLine("Time, Fails");
+
+        writer.WriteLine($"{_time}, {_failCount}");
+
+        writer.Close();
+    }
+
 }
